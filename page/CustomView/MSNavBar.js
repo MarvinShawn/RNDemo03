b2973a60ad940e5e8cfeda1235664658fc3194d4
Marvin <marvinshawn9333@gmail.com>
2017-02-27 23:59:37 +0800

    //注释

--- page/CustomView/MSNavBar.js
+++ page/CustomView/MSNavBar.js
@@ -16,8 +16,9 @@ export default class MSNavBar extends Component {
   render(){
     const {title,leftTitle,leftImage,leftAction,rightTitle,rightImage,rightAction,barTinColor} = this.props;
     return(
+        //在冲突值的情况下，从最右边元素的值将会优先，并且 falsy 值如 false，undefined 和 null 将被忽略。一个常见的模式是基于某些条件有条件地添加一个样式
       <View style = {[styles.container,barTinColor && {backgroundColor:barTinColor}]}>
-        //在冲突值的情况下，从最右边元素的值将会优先，并且 falsy 值如 false，undefined 和 null 将被忽略。一个常见的模式是基于某些条件有条件地添加一个样式。
+
 
       </View>
 
