e804cac5215ee92d3241153289f92e58e3cec078
Marvin <marvinshawn9333@gmail.com>
2017-03-05 00:21:20 +0800

    网络请求数据并展示，下一步做上拉加载更多数据

--- page/NetWorkPage.js
+++ page/NetWorkPage.js
@@ -24,20 +24,6 @@ export default class NetWorkPage extends Component {
     }
   }
 
-  buttonClick = () => { //箭头函数 省略了绑定this的操作
-      fetch('http://open3.bantangapp.com/recommend/index?app_id=com.jzyd.BanTang&app_installtime=1462985294&app_versions=5.7&channel_name=appStore&client_id=bt_app_ios&client_secret=9c1e6634ce1c5098e056628cd66a17a5&last_get_time=1463238932&os_versions=9.3.1&screensize=750&track_device_info=iPhone8&track_deviceid=476C2ABB-9058-4621-930B-158CBB91354B&v=12&page=0&pagesize=20',{method:'GET'})
-      .then((response) => response.json())
-      .then((responseData)=>{
-      this.setState({
-
-      });
-    }).catch((error)=>{
-        console.log(error);
-    })
-  };
-
-
-
   render(){
     return(
       <Navigator
@@ -54,15 +40,24 @@ class NetView extends Component {
 
     super(props);
     var ds = new ListView.DataSource({rowHasChanged:(r1,r2)=>r1 !== r2});
-    this.arr = [{'title':'程博士我们走','likes':'125'}],
+    this.arr = [{'title':'程皮皮我们走','likes':'125'}],
     this.state = {
         dataSource:ds.cloneWithRows(this.arr),
         isRefreshing:false,
+        isDataOk:false
     }
+  }
+
+  componentDidMount(){    //  4.0组件加载完毕调用
+    this._refreshData();
   }
 
   leftBarbuttonAction = ()=>{
     console.log('来这儿了');
+    this.setState({
+
+      isDataOk:true,
+    });
     this.refs.NavBar.setState({
       color:'#ab2626'
     });
@@ -74,7 +69,7 @@ class NetView extends Component {
     return(
       <TouchableHighlight  >
       <View style = {styles.cellStyle}>
-        <Image style = {styles.imageSize} source={require('../page/img/cellImage.png')}/>
+        <Image style = {styles.imageSize} source={{uri:this.arr[rowID]['pic']}}/>
         <Text style = {styles.TextStyle1}>{this.arr[rowID]['title']}</Text>
         <Text style = {styles.TextStyle2}>💗{this.arr[rowID]['likes']}</Text>
       </View>
@@ -99,6 +94,7 @@ class NetView extends Component {
         });
     }).catch((error)=>{
 
+      alert(error);
         this.arr.push({'title':'皮皮虾，我们走','likes':'1235'})
         this.setState({
           isRefreshing:false,
@@ -107,6 +103,14 @@ class NetView extends Component {
         console.log(error);
     })
   };
+
+//滚到底部的代理方法(onEndReachedThreshold)
+  _onEndReached = ()=>{
+
+    if (this.state.isDataOk) {
+      console.log('滚到了底部');
+    }
+  }
 
 
   render(){
@@ -126,6 +130,9 @@ class NetView extends Component {
               tintColor = "#FF0000"
               title = "加载中"
           />}
+          onEndReached = {this._onEndReached()}
+          onEndReachedThreshold={0}
+          // onEndReachedThreshold = {20}//调用onEndReached之前的临界值
           automaticallyAdjustContentInsets={false} //自动偏移20像素的原因
           style= { styles.listView }
           dataSource={this.state.dataSource}
